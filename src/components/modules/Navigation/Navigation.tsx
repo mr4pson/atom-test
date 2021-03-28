2988fd0003b4568e83f3ce067a8460008bfc59db
Vyacheslav Goryachkin <vgoryachkin@kameleoon.com>
2021-03-28 22:07:03 +0300

    add removeJwtPair method for authButton

--- src/components/modules/Navigation/Navigation.tsx
+++ src/components/modules/Navigation/Navigation.tsx
@@ -13,7 +13,6 @@ import Icon from "components/uiKit/Icon";
 import { arrowUpIcon, logoutIcon, userInfoIcon } from "icons";
 import { useHistory, useLocation } from "react-router";
 import { Page, paths } from "routes/constants";
-import { removeJwtPair } from "components/pages/LoginPage/helpers";
 
 type Props = {
   navigationType: NavigationType;
@@ -52,7 +51,6 @@ function Navigation(props: Props): JSX.Element {
             path={logoutIcon.path}
             viewBox={logoutIcon.viewBox}
             title="AtomTest"
-            onClick={removeJwtPair}
           />
         );
       }
